2a90e6129efced2ece367394e2d7eb089d9f5bd8
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2019-10-10 19:47:38 -0400

    added Drawer
    navigation drawer titles, icons and close action

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -29,6 +29,73 @@ class _HomeState extends State<Home> {
         ],
       ),
 
+
+      drawer: new Drawer(
+
+        child: new ListView(
+
+          children: <Widget>[
+
+            new UserAccountsDrawerHeader(
+                accountName: new Text("Jules Frantz Stephane Loubeau"),
+                accountEmail: new Text("hey@jfsl.me"),
+              decoration: new BoxDecoration(
+                color: Colors.purple
+
+              ),
+
+            ), 
+            
+            new ListTile(
+              title: new Text("Home"),
+              leading: new Icon(Icons.add, color: Colors.purple),
+              trailing: new Icon(Icons.search, color: Colors.purple),
+              
+            ),
+
+            new ListTile(
+              title: new Text("Home"),
+              leading: new Icon(Icons.add, color: Colors.purple),
+              trailing: new Icon(Icons.search, color: Colors.purple),
+
+            ),
+
+            new ListTile(
+              title: new Text("Home"),
+              leading: new Icon(Icons.add, color: Colors.purple),
+              trailing: new Icon(Icons.search, color: Colors.purple),
+
+            ),
+
+            new ListTile(
+              title: new Text("Home"),
+              leading: new Icon(Icons.add, color: Colors.purple),
+              trailing: new Icon(Icons.search, color: Colors.purple),
+
+            ),
+
+            new Divider(
+              height: 10.0,
+              color: Colors.purple,
+            ),
+
+            new ListTile(
+              title: new Text("Close"),
+              leading: new Icon(Icons.add, color: Colors.purple),
+              trailing: new Icon(Icons.close, color: Colors.purple),
+              onTap: (){
+
+                Navigator.of(context).pop(); 
+
+              },
+
+            )
+
+
+          ],
+        ),
+      ),
+
     );
   }
 }
